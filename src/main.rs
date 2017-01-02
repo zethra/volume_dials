6cb964b77730bd632334a69ebb4756b8da9d63dc
zethra <jediben97@gmail.com>
2017-01-02 04:07:14 -0500

    Added better error checking

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate alsa;
 extern crate serial;
 
-use std::io;
 use std::time::Duration;
 
 use std::io::prelude::*;
@@ -14,27 +13,56 @@ use alsa::mixer::{
 };
 
 fn main() {
-    let mixer = Mixer::new("default", false).unwrap();
+    let mixer = match Mixer::new("default", false) {
+        Ok(mixer) => mixer,
+        Err(_) => {
+            println!("Couldn't get alsa mixer");
+            return
+        },
+    };
     let selem_id = SelemId::new("Master", 0);
-    let selem = mixer.find_selem(&selem_id).unwrap();
-    let (min, max) = selem.get_playback_volume_range();
+    let selem = match mixer.find_selem(&selem_id) {
+        Some(selem) => selem,
+        None => {
+            println!("Couldn't get selem Master");
+            return
+        }
+    };
+    let (_, max) = selem.get_playback_volume_range();
 
-
-    let mut port = serial::open("/dev/ttyACM0").unwrap();
-    port.reconfigure(&|settings| {
-        try!(settings.set_baud_rate(serial::Baud9600));
+    let mut port = match serial::open("/dev/ttyACM1") {
+        Ok(port) => port,
+        Err(_) => {
+            println!("Could not open serial port");
+            return
+        },
+    };
+    if port.reconfigure(&|settings| {
+        if settings.set_baud_rate(serial::Baud9600).is_err() { println!("Couldn't set baud rate"); }
         settings.set_char_size(serial::Bits8);
         settings.set_parity(serial::ParityNone);
         settings.set_stop_bits(serial::Stop1);
         settings.set_flow_control(serial::FlowNone);
         Ok(())
-    });
-    port.set_timeout(Duration::from_millis(1000));
+    }).is_err() {
+        println!("Couldn't configure serial port");
+        return
+    };
+    if port.set_timeout(Duration::from_millis(1000)).is_err() {
+        println!("Couldn't set port timeout");
+        return
+    }
+
     let mut buf: Vec<u8> = vec![0];
     loop {
-        port.read(&mut buf[..]);
+        if port.read(&mut buf[..]).is_err() {
+            println!("Couldn't read from serial port");
+            continue
+        }
         for ch in SelemChannelId::all() {
-            selem.set_playback_volume(*ch, buf[0] as i64 * max / 100);
+            if selem.set_playback_volume(*ch, buf[0] as i64 * max / 100).is_err() {
+                println!("Couldn't change volume")
+            }
         }
     }
 }
